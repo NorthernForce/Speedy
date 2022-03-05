4a7a3b12312eb330553efff437148a054300962e
ADever <aiden.dever@gorhamschools.org>
2022-03-05 15:48:48 -0500

    Added pivot POV controls to driver controller

--- src/main/cpp/OI.cpp
+++ src/main/cpp/OI.cpp
@@ -31,6 +31,7 @@ void OI::MapControllerButtons() {
     SimpleButton(driverController, Xbox::lt_bumper).WhileHeld(new IntakeBall);
     SimpleButton(driverController, Xbox::rt_bumper).WhileHeld(new PushOutBall);
     SimpleButton(driverController, Xbox::X_button).WhenPressed(new MoveToCoordinate(24, 0));
+    SimplePOV(driverController, XboxPOV::up).WhenPressed(new PivotToggle);
 
     //manipulator
     SimpleButton(manipulatorController, Xbox::X_button).WhileHeld(new LowerClimber);
