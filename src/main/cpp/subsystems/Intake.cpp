c8e0415d3c23eba4a2ffc9880aa2cfd8b2316eae
Hunter-Pellerin <hpellerin2003@gmail.com>
2022-03-12 10:15:41 -0500

    10:15 changes intake top slower

--- src/main/cpp/subsystems/Intake.cpp
+++ src/main/cpp/subsystems/Intake.cpp
@@ -15,7 +15,7 @@ Intake::Intake() {
 }
 
 void Intake::Run(bool reverse) {
-    intakeTopSpark->Set(reverse ? -1.0 : 1.0);
+    intakeTopSpark->Set(reverse ? -0.9 : 0.9);
     intakeBottomSpark->Set(reverse ? 1.0 : -1.0);
 }
 
