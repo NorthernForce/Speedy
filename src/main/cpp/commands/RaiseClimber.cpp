dd40d02bc759f6da98e19707ac7e48a802b9c73f
ADever <aiden.dever@gorhamschools.org>
2022-03-30 19:06:11 -0400

    Fixed Climber

--- src/main/cpp/commands/RaiseClimber.cpp
+++ src/main/cpp/commands/RaiseClimber.cpp
@@ -12,9 +12,7 @@ RaiseClimber::RaiseClimber() {
 
 // Called repeatedly when this Command is scheduled to run
 void RaiseClimber::Execute() {
-  if (!RobotContainer::climber->TooTall()) {
     RobotContainer::climber->Raise();
-  }
 }
 
 void RaiseClimber::End(bool interrupted) {
@@ -22,5 +20,5 @@ void RaiseClimber::End(bool interrupted) {
 }
 // Returns true when the command should end.
 bool RaiseClimber::IsFinished() {
-  return RobotContainer::climber->TooTall();
+  return false;
 }
